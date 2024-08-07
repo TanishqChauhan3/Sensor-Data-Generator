b61a27b75a551976bbe9d1bb4245847dc4aa6ab6
Tanishq Chauhan <tanishqchauhan999@gmail.com>
2024-08-07 23:58:21 +0530

    Update Iot.py

--- src/Iot.py
+++ src/Iot.py
@@ -3,19 +3,21 @@ import random
 import time
 
 def data():
-    data = {
-        "temperature": random.uniform(-10, 40),  
-        "humidity": random.uniform(0, 100)       
+    return {
+        "temperature": random.uniform(-10, 40),
+        "humidity": random.uniform(0, 100)
     }
-    return data
 
 def save_to_json(data, filename='sensor_data.json'):
     with open(filename, 'w') as json_file:
         json.dump(data, json_file, indent=4)
 
 if __name__ == "__main__":
-    while True:
-        sensor_data = data()
-        save_to_json(sensor_data)
-        print("Generated and saved data:", sensor_data)
-        time.sleep(5) 
+    try:
+        while True:
+            sensor_data = data()
+            save_to_json(sensor_data)
+            print("Generated and saved data:", sensor_data)
+    
+    except KeyboardInterrupt:
+        print("Data generation stopped by user.")
